bd1380c684c9d108d13f1912284948128d0d03f3
nik_druz <nik.druzhckov@yandex.ru>
2020-02-27 14:54:15 +0300

    first commit test

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: 'env'
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                minimize: true,
+                url: false
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+    ]
+  },
+  plugins: [
+      new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
+  ]
+};
